e0fceee65e6bf7c6809721139cb1b1833d9b1c8a
famouscat <onismaa@gmail.com>
2023-02-01 21:59:25 +0800

    add two new views

---
+++ src/views/EventPlaza.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>event plaza page</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ src/views/NftFinance.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>nft finance page</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
