66b401e18ccdb562e4666a418287058304f9965d
famouscat <onismaa@gmail.com>
2023-02-27 16:54:10 +0800

    update

--- src/views/MintNFT.vue
+++ src/views/MintNFT.vue
@@ -1,59 +1,37 @@
 <template>
-  <div class="grid grid-cols-12 overflow-clip">
-    <div
-      class="col-start-3 col-end-8 h-full flex flex-shrink-0 justify-center items-center"
-    >
-      <div>dsaidojsaidsaj</div>
-    </div>
-    <div
-      class="testbg col-start-8 col-end-11 max-h-[80vh] overflow-y-scroll overscroll-y-contain flex flex-shrink-0 flex-col justify-start items-center"
-    >
-      <SectionWrapper :title="t('labels.avatarShape')">
-        <div class="h-[100px]">Content</div>
-      </SectionWrapper>
-      <div>
-        <h3>Background Color</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Face</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Tops</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Ear</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Earrings</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Eyebrows</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Eyes</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Noses</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
-      <div>
-        <h3>Glasses</h3>
-        <div class="h-[100px]">Content</div>
-      </div>
+  <div class="root">
+    <div class="container">
+      <div class="preview">dsaidojsaidsaj</div>
+      <div class="btn">button</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
-import SectionWrapper from "../components/SectionWrapper.vue";
 
 const { t } = useI18n();
 </script>
+
+<style scopeds lang="scss">
+.root {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 100vh;
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 48px;
+
+    .preview {
+      width: clamp(2rem, 38vw, 200rem);
+      aspect-ratio: 2 / 3;
+      background: rgb(114 113 113);
+    }
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import dynamicImport from "vite-plugin-dynamic-import";
 import stylelint from "vite-plugin-stylelint";
-
+import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dynamicImport(), stylelint()],
+  plugins: [vue(), vueJsx(), dynamicImport(), stylelint(), vuetify()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
