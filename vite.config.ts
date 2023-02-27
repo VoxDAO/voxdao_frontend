58dbeeac24c0c2d542983352646c9bfc2ed4b0cf
famouscat <onismaa@gmail.com>
2023-02-27 17:37:38 +0800

    update: remove vuetify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import dynamicImport from "vite-plugin-dynamic-import";
 import stylelint from "vite-plugin-stylelint";
-import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dynamicImport(), stylelint(), vuetify()],
+  plugins: [vue(), vueJsx(), dynamicImport(), stylelint()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
